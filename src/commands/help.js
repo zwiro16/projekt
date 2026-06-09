44c04dad12fe29e7024614f216d180b55a346885
zwiro16 <rzwirek16@gmail.com>
2026-06-09 18:46:07 +0200

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Ping command'),
+
+  async execute(interaction) {
+    await interaction.reply('Pong!');
+  }
+};
